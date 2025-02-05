84071e38297c6c69fb4c2962d473880e45665c73
Nando <ernandofebrian6@gmail.com>
2025-02-05 15:48:28 +0700

    Done making card show up when add the book.

--- js-file.js
+++ js-file.js
@@ -15,9 +15,45 @@ function addBookToLibrary(Book) {
     myLibrary.push(Book);
 }
 
+function makeCard(obj) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    
+    const title = document.createElement("p");
+    title.classList.add("title-card");
+    title.textContent = `Title Book: ${obj.title}`;
+    card.appendChild(title);
+
+    const author = document.createElement("p");
+    author.classList.add("author-card");
+    author.textContent = `Author: ${obj.author}`;
+    card.appendChild(author);
+
+    const page = document.createElement("p");
+    page.classList.add("page-card");
+    page.textContent = `Page Number: ${obj.page}`;
+    card.appendChild(page);
+
+    const status = document.createElement("p");
+    status.classList.add("status-card");
+    status.textContent = `Status Reading: ${obj.read}`;
+    card.appendChild(status);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-button");
+    removeBtn.textContent = "Delete this card!";
+    card.appendChild(removeBtn);
+
+    return card
+}
+
 function showCard() {
+    const container = document.querySelector(".card-container");
+    container.textContent = '';
+
     for (const book of myLibrary) {
-        
+        const bookCard = makeCard(book);
+        container.appendChild(bookCard);
     }
 
     console.table(myLibrary);
@@ -57,7 +93,17 @@ closeBtn.addEventListener("click", () => {
     const newBook = new Book(titleBook, authorBook, pageBook, statusBook);
     addBookToLibrary(newBook);
 
+    title.value = "";
+    author.value = "";
+    page.value = "";
+    status.value = "";
+
     dialog.close();
     showCard()
 });
 
+const removeBtn = document.querySelector(".remove-button");
+
+removeBtn.addEventListener(("click"), () => {
+    alert("anjing bingung gw");
+});
